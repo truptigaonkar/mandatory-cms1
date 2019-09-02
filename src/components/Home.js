9a941ef0781e6d51b2f5647785ae80b20dc5d503
truptigaonkar <trups11.trupti@gmail.com>
2019-09-02 18:36:08 +0200

    React Markdown package installed for code  and Syntax Highlighting, substring used extracts the characters from a string, between two specified indices

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 const API_URL_ARTICLE = "http://192.168.99.102:8080/api/collections/get/articles";
 
@@ -33,9 +34,11 @@ const Home = () => {
                         <div class="card-body" style={{ padding: "5px" }}>
                             <p key={article._id}>
                                 <h6><Link to={`/article/${article._id}`}>{article.title}</Link></h6>
-                                <p class="card-text" style={{ fontSize: "12px" }}>{article.body} <Link to={`/article/${article._id}`}>Read more &rarr;</Link></p>
+                                <p class="card-text" style={{ fontSize: "12px" }}>
+                                    <ReactMarkdown source={article.body.substring(0, 50)} />
+                                    <Link to={`/article/${article._id}`}>Read more &rarr;</Link>
                                 <div class=" text-muted" style={{ fontSize: "12px", float: "right" }}>Published by: <b>{article.author[0].display}</b> on
-                                <b> {article.published_on}</b></div>
+                                <b> {article.published_on}</b></div></p>
                             </p>
                         </div>
                     </div>
